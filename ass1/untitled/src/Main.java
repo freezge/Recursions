6dd59753e75e96fee4fef0623b9f2a89642a22f6
freezge <freezge@gmail.com>
2023-04-06 08:47:54 +0600

    task1

--- ass1/untitled/src/Main.java
+++ ass1/untitled/src/Main.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
-
 public class Main {
+    /*
+    * finding min value through recursion
+    * @param arr - array
+    * @param n - lenght
+    * @param sc - scanner for taking input from console
+    * @param i - to navigate in recursion
+    * @param min - for minimum
+    * */
     public static int fmin(int[] arr, int i){//function for task1
         if(i == arr.length - 1){//checking if we are on the last element of array
             return arr[i];// returning last value
@@ -15,11 +22,10 @@ public class Main {
         System.out.println("Please enter length of an array");
         int n = sc.nextInt();//length of an array
         int[] arr = new int[n];//array from condition
-        for(int i = 0; i < n; i++){
-            arr[i]=sc.nextInt();//reading array
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();//reading array
         }
         int min = fmin(arr, 0);//calling for function to find minimum
         System.out.println("Min value is " + min);//output minimum
     }
-
 }
